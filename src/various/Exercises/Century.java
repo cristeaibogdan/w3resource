0fcf2bb4bc1188124942f94c746ba8ca3e775c39
Cristea Bogdan <cristea.ibogdan@gmail.com>
2022-09-28 19:58:11 +0300

    Modified Century.java

--- src/various/Exercises/Century.java
+++ src/various/Exercises/Century.java
@@ -1,5 +1,4 @@
 package various.Exercises;
-
 import java.util.Scanner;
 
 public class Century {
@@ -12,24 +11,24 @@ public class Century {
     2030 => 21
      */
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
 
         System.out.println("Input the year: ");
         int year = input.nextInt();
 
-        int century = year/100;
-        int remains = year%100;
+        int century = year / 100;
+        int remains = year % 100;
 
         //System.out.println("Rounded down is: "+century);
         //System.out.println("Remains is: "+numberrest);
 
-        if (remains != 0 ) {
-            System.out.println("We are in century: "+(century+1));
+        if (remains != 0) {
+            System.out.println("We are in century: " + (century + 1));
         }
 
-        if (remains == 0 ) {
-            System.out.println("We are in century: "+(century));
+        if (remains == 0) {
+            System.out.println("We are in century: " + (century));
         }
+
     }
 }
